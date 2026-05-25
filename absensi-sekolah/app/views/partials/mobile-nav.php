8caddac999ba47d21fbfdf17c8f723c8243e8e29
bside2085 <bside2085@gmail.com>
2026-05-26 05:44:43 +0800

    hidding absen on navbar

--- absensi-sekolah/app/views/partials/mobile-nav.php
+++ absensi-sekolah/app/views/partials/mobile-nav.php
@@ -4,6 +4,17 @@
   try {
       $unreadNav = (new \App\Models\NotificationRead())->unreadCountFor((int)$u['id']);
   } catch (\Throwable $e) {}
+
+  $uri = parse_url($_SERVER['REQUEST_URI'] ?? '/', PHP_URL_PATH);
+  $scriptDir = rtrim(str_replace('\\', '/', dirname($_SERVER['SCRIPT_NAME'] ?? '')), '/');
+  if ($scriptDir !== '' && $scriptDir !== '/' && str_starts_with($uri, $scriptDir)) {
+      $uri = substr($uri, strlen($scriptDir));
+      if ($uri === '') {
+          $uri = '/';
+      }
+  }
+  $uri = rtrim($uri, '/') ?: '/';
+  $hideAbsenButton = $uri === '/absensi';
 ?>
 <nav class="mobile-bottom-nav">
   <a href="<?= url('/dashboard') ?>" class="<?= is_active('/dashboard') ?>">
@@ -12,9 +23,11 @@
   <a href="<?= url('/absensi/riwayat') ?>" class="<?= is_active('/absensi/riwayat') ?>">
     <i class="bi bi-clock-history"></i><span>Riwayat</span>
   </a>
+  <?php if (!$hideAbsenButton): ?>
   <a href="<?= url('/absensi') ?>" class="center">
     <i class="bi bi-camera-fill"></i><span>Absen</span>
   </a>
+  <?php endif; ?>
   <a href="<?= url('/cuti') ?>" class="<?= is_active('/cuti') ?>">
     <i class="bi bi-calendar-event"></i><span>Cuti</span>
   </a>
